2cb0b655dce9ea2e0a64496e916c9b9eeb9109a7
Aakanksha Sharma <mymac@Aakankshas-MacBook-Air.local>
2017-10-09 14:34:00 +0530

    version upgrade to v1.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "sib-api-v3-sdk"
-VERSION = "1.0.2"
+VERSION = "1.1.0"
 # To install the library, run the following
 #
 # python setup.py install
