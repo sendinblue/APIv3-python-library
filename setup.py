d3125409059b8566301ac8653c02a417e00be463
Rudhishthir Prakash <rudhishthir.prakash@sendinblue.com>
2020-10-16 15:09:12 +0530

    AP-1036: Update version of the package

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "sib-api-v3-sdk"
-VERSION = "6.3.0"
+VERSION = "6.3.1"
 # To install the library, run the following
 #
 # python setup.py install
